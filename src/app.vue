da3b300676cacb3341f1cd71f159cfc012758822
liaobo <>
2019-01-24 11:02:31 +0800

    add

--- src/app.vue
+++ src/app.vue
@@ -1,10 +1,10 @@
 <template>
-  <div id="app">
-    <div id="cover"></div>
-    <Header></Header>
-    <todo></todo>
-    <Footer></Footer>
-  </div>
+    <div id="app">
+        <div id="cover"></div>
+        <Header></Header>
+        <Todo></Todo>
+        <Footer></Footer>
+    </div>
 </template>
 
 <script>
@@ -13,32 +13,29 @@ import Footer from './todo/footer.jsx'
 import Todo from './todo/todo.vue'
 
 export default {
-  components: {
-    Header,
-    Footer,
-    Todo,
-  }
+    components: {
+        Header,
+        Footer,
+        Todo
+    }
 }
 </script>
 
 <style lang="stylus" scoped>
-#app{
-  position absolute
-  left 0
-  right 0
-  top 0
-  bottom 0
-}
-#cover{
-  position absolute
-  left 0
-  top 0
-  right 0
-  bottom 0
-  background-color #999
-  opacity .9
-  z-index -1
-}
+#app
+    position absolute
+    left 0
+    right 0
+    top 0
+    bottom 0
+    #cover
+        position absolute
+        left 0
+        right 0
+        top 0
+        bottom 0   
+        background-color #999 
+        opacity 0.2
+        z-index -1
+
 </style>
-
-
